408824b8a10419ae108d17b7b1771f423116f1f1
Frank Denzer <frank.denzer@ard.de>
2024-07-16 16:56:27 +0200

    add team objects

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,35 +1,26 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-import svlvolleyscore.composeapp.generated.resources.Res
-import svlvolleyscore.composeapp.generated.resources.compose_multiplatform
+object TeamMagenta : Team(Color(0xFFE040FB))
+object TeamCyan : Team(Color(0xFF40C4FF))
+
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+            TeamMagenta()
+            TeamCyan()
         }
     }
 }
+
+import androidx.compose.ui.graphics.Color
+
+data class ScoreEntry(val teamAColor: Color, val teamAScore: Int, val teamBColor: Color, val teamBScore: Int)
